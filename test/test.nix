06c48f2927caa0da8896e341fbb66b388c4e87d5
kalipso <kalipso@c3d2.de>
2023-10-31 16:43:02 +0100

    [nix] update test to run quicker

--- test/test.nix
+++ test/test.nix
@@ -1,8 +1,6 @@
-# ./tests/hello-world-server.nix
 (import ./lib.nix) {
   name = "gokill-base-test";
   nodes = {
-    # `self` here is set by using specialArgs in `lib.nix`
     node1 = { self, pkgs, ... }: {
       imports = [ self.nixosModules.gokill ];
 
@@ -13,7 +11,7 @@
             type = "Timeout";
             name = "custom timeout";
             options = {
-              duration =  10;
+              duration =  3;
             };
             actions = [
               {
@@ -34,9 +32,9 @@
     import time
     start_all() # wait for our service to start
     node1.wait_for_unit("gokill")
-    time.sleep(11)
+    time.sleep(4)
     output = node1.succeed("journalctl -u gokill.service | tail -n 2 | head -n 1")
     # Check if our webserver returns the expected result
-    assert "hellow world" in output
+    assert "hello world" in output
   '';
 }
