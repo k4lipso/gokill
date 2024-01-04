1448da48c994ab23994f06f48ed027bb08d5ce0d
kalipso <kalipso@c3d2.de>
2024-01-04 17:40:50 +0100

    [trggers] WIP rework

--- triggers/triggers.go
+++ triggers/triggers.go
@@ -139,7 +139,7 @@ func NewTrigger(config internal.KillSwitchConfig) (*TriggerHandler, error) {
 		}
 	}
 
-	return nil, fmt.Errorf("Error parsing config: Trigger with type %s does not exists", config.Type)
+	return TriggerHandler{}, fmt.Errorf("Error parsing config: Trigger with type %s does not exists", config.Type)
 }
 
 func GetAllTriggers() []Trigger {
