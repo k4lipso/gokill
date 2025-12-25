b8f2b40cc13d82f1f5fd00b17bd0ef4e96bbbafe
kalipso <kalipso@c3d2.de>
2025-12-25 03:32:06 +0100

    [nix] add remote module options

--- nixos-modules/gokill.nix
+++ nixos-modules/gokill.nix
@@ -2,8 +2,14 @@
 let
   cfg = config.services.gokill;
   configFile = pkgs.writeText "config.json" (builtins.toJSON cfg.triggers); 
-  gokill-pkg = inputs.gokill.packages.aarch64-linux.gokill;
+  remoteConfigFile = pkgs.writeText "remote-config.json" (builtins.toJSON cfg.remote.config); 
+  gokill-pkg = inputs.self.packages.x86_64-linux.gokill;
   testRun = if cfg.testRun then "-t" else "";
+  #remoteCfg = if cfg.remote.config != {} then "-remote-config ${remoteConfigFile}" else "";
+  remoteCfg = "";
+  remote = if cfg.remote.enable then "-r" else "";
+  keyAge = if cfg.remote.ageKeyFile != "" then "-key-age ${cfg.remote.ageKeyFile}" else "";
+  keyP2p = if cfg.remote.p2pKeyFile != "" then "-key-p2p ${cfg.remote.p2pKeyFile}" else "";
 in
 {
   options = with lib; {
@@ -23,6 +29,76 @@ in
           When set to true gokill is performing a test run
           '';
       };
+
+      remote = {
+        enable = mkOption {
+          default = false;
+          type = types.bool;
+          description = mdDoc ''
+            Enables gokills remote handler
+            '';
+        };
+
+        ageKeyFile = mkOption {
+          type = with lib.types; either path str;
+          default = "";
+          description = mdDoc ''
+            Path to gokill age key
+            '';
+        };
+
+        p2pKeyFile = mkOption {
+          type = with lib.types; either path str;
+          default = "";
+          description = mdDoc ''
+            Path to gokill p2p key
+            '';
+        };
+
+        #remoteConfigFile = mkOption {
+        #  type = types.str;
+        #  default = "";
+        #  description = mdDoc ''
+        #    Path to gokill remote config
+        #    '';
+        #};
+      };
+
+      #remote.config = {
+      #  id = mkOption {
+      #    type = types.str;
+      #  };
+      #  key = mkOption {
+      #    type = types.str;
+      #  };
+      #  groups = mkOption {
+      #    description = "gokill remote config";
+      #    default = {};
+      #    type = with types; types.listOf ( submodule {
+      #      options = {
+      #        Name = mkOption {
+      #          type = types.str;
+      #        };
+      #        Id = mkOption {
+      #          type = types.str;
+      #        };
+      #        Peers = mkOption {
+      #          description = "list of peers";
+      #          type = with types; types.listOf ( submodule {
+      #            options = {
+      #              id = mkOption {
+      #                type = types.str;
+      #              };
+      #              key = mkOption {
+      #                type = types.str;
+      #              };
+      #            };
+      #          });
+      #        };
+      #      };
+      #    });
+      #  };
+      #};
 
       triggers = mkOption {
         description = "list of triggers";
@@ -61,7 +137,7 @@ in
             };
           };
         });
-      };
+};
     };
   };
 
@@ -83,12 +159,16 @@ in
       description = "gokill daemon";
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${gokill-pkg}/bin/gokill --db /etc/gokill -r -c ${configFile} ${testRun}";
+        ExecStart = "${gokill-pkg}/bin/gokill --db /etc/gokill ${remote} ${keyAge} ${keyP2p} ${remoteCfg} -c ${configFile} ${testRun}";
         Restart = "on-failure";
       };
 
       wantedBy = [ "default.target" ];
     };
+
+    environment.systemPackages = [
+      gokill-pkg
+    ];
   };
 }
 
