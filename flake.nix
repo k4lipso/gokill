c072997172e9075d50ab1d0d74bf0539cc41603d
kalipso <kalipso@c3d2.de>
2026-04-02 01:27:26 +0200

    [nixpkgs] update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, utils, ... }: 
